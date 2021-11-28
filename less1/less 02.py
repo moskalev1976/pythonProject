d82182740e8816e6d75668fa30a0b0eca9e08ec8
moskalev1976 <qAakZnXpT9nfBEH>
2021-11-28 17:03:09 +0300

    Merge remote-tracking branch 'origin/Less1' into Less1

--- less1/less 02.py
+++ less1/less 02.py
@@ -1,3 +1,7 @@
-test_01 = input("Введите любое время в секундах: ")
-test_02 = int(test_01)/60
-print(f"Время (ч:м) \n {int(test_02)}:{int(round(test_02%1,2)*60)}")
+test_01 = int(input("Введите любое время в секундах: "))
+if test_01 < 60:
+    print(f"Время (c): {test_01:02}")
+elif int(test_01) < 3600:
+    print(f"Время (м:c): {test_01 // 60:02}: {test_01 % 60:02}")
+else:
+    print(f"Время (ч:м:c): {test_01 // 3600:02}:{(test_01 % 3600) // 60:02}:{test_01 % 60:02}")
